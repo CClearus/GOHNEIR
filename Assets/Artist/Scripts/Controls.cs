7d628500fb68286bf954020f7d1d4e4902c454d9
Patthon Rayubsang <tistplays584yt@gmail.com>
2026-07-24 16:01:22 +0700

    Basic Movement in Test scene

---
+++ Assets/Artist/Scripts/Controls.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System;
+
+public class Controls : MonoBehaviour
+{
+    [SerializeField] float mouseSensitivity = 1f;
+    Vector3 rotation;
+    Movement movement;
+
+    void Start()
+    {
+        rotation = Vector3.zero;
+        Cursor.lockState = CursorLockMode.Locked;
+        movement = GetComponent<Movement>();
+    }
+
+    void Update()
+    {
+
+        // Look
+        rotation.x -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        rotation.y += Input.GetAxis("Mouse X") * mouseSensitivity;
+        rotation.x = Mathf.Clamp(rotation.x, -90f, 90f);
+        transform.rotation = Quaternion.Euler(rotation);
+
+        // Movement
+        int rLDir = Convert.ToInt32(Input.GetKey(KeyCode.D)) - Convert.ToInt32(Input.GetKey(KeyCode.A));
+        int fBDir = Convert.ToInt32(Input.GetKey(KeyCode.W)) - Convert.ToInt32(Input.GetKey(KeyCode.S));
+        Vector3 dir = new Vector3(rLDir, 0f, fBDir).normalized;
+        movement.Move(Quaternion.Euler(0f, rotation.y, 0f) * dir);
+
+        // Jump
+        if (Input.GetKeyDown(KeyCode.Space) && Physics.Raycast(transform.position, Vector3.down, 1f, LayerMask.GetMask("Default")))
+        {
+            Debug.Log("Space Pressed");
+            movement.Jump();
+        }
+
+    }
+}
---
+++ Assets/Artist/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody body;
+    [SerializeField] float speed = 1f;
+    [SerializeField] float jumpForce = 10f;
+    
+    public void Move(Vector3 dir)
+    {
+        body.linearVelocity -= Vector3.ProjectOnPlane(body.linearVelocity, Vector3.up);
+        body.linearVelocity += dir * speed;
+    }
+
+    public void Jump()
+    {
+        body.AddForce(Vector3.up * jumpForce);
+    }
+}
